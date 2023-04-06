547c577ef2d8899a8a6038a90bc57f964c33edae
lhk3337 <lhk3337@naver.com>
2023-04-06 12:11:56 +0900

    [FEAT] add player

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,3 +1,14 @@
 import "../scss/styles.scss";
 
-console.log("hi");
+const $btn = document.querySelectorAll(".btn");
+const $player = document.querySelector(".player");
+$btn.forEach((value) => {
+  const youtubeId = value.getAttribute("data-id");
+  value.addEventListener("click", () => {
+    $player.innerHTML = `
+      <div class="video_container">
+        <iframe width="450" height="270" src="https://www.youtube.com/embed/${youtubeId}?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      </div>
+    `;
+  });
+});
