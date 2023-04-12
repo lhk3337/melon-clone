c32be4c08309357ca09be05edd055e8c71ce19e4
lhk3337 <lhk3337@naver.com>
2023-04-12 19:19:51 +0900

    [FIX] dummy post 삭제 하기

--- src/controllers/songController.js
+++ src/controllers/songController.js
@@ -11,21 +11,3 @@ export const home = async (req, res) => {
     return res.render("server-error");
   }
 };
-
-export const posthome = async (req, res) => {
-  request(
-    `https://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=kpop&limit=50&api_key=${process.env.API_KEY}&format=json&period=7day`,
-    function (error, response, body) {
-      if (!error && response.statusCode === 200) {
-        const data = JSON.parse(body);
-        const addDatas = data.tracks.track.map((v) => {
-          return { ...v, listners: Math.floor(Math.random() * (1000 - 1) + 1) };
-        });
-        Song.collection.insertMany(addDatas);
-      } else {
-        console.log(error.message);
-        res.status(500).send("Server Error");
-      }
-    }
-  );
-};
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -4,5 +4,4 @@ import { home, posthome } from "../controllers/songController";
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
-rootRouter.post("/", posthome);
 export default rootRouter;
