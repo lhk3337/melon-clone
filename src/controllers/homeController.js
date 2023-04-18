b5c6186a3ddabc9197562964ddb52665370aae7b
lhk3337 <lhk3337@naver.com>
2023-04-18 22:48:05 +0900

    [FEAT] main page 추가

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,3 +1,18 @@
+import Song from "../models/Song";
+import User from "../models/User";
+
 export const home = async (req, res) => {
-  return res.render("home", { pageTitle: "Home" });
+  const {
+    session: {
+      user: { _id },
+    },
+  } = req;
+
+  const toplists = await Song.find({}).sort({ playcount: -1 }).limit(30);
+  const playlists = await User.findById(_id).populate("playlists").limit(30);
+
+  const toplistsData = JSON.parse(JSON.stringify(toplists));
+  const playlistData = JSON.parse(JSON.stringify(playlists));
+
+  return res.render("home", { pageTitle: "Home", toplistsData, playlistData });
 };
